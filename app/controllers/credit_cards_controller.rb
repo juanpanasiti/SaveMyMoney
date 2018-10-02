a3daef99fa8d139df41d2308b18e3857b37c8a38
Juan M. Panasiti <juanpanasiti@gmail.com>
2018-10-02 12:29:48 -0300

    Configurado altas y bajas de CreditCard

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,49 @@
+class CreditCardsController < ApplicationController
+  before_action :set_credit_card, only: [:edit, :update, :destroy]
+  before_action :options_for_select, only: [:new, :create, :edit, :update]
+  def index
+    @le_titule = "Tarjetas de Crédito"
+    @credit_cards = current_user.credit_cards
+  end#index
+
+  def new
+    @credit_card = CreditCard.new
+  end#new
+  def create
+    @credit_card = current_user.credit_cards.new(credit_card_params)
+    respond_to do |format|
+      if @credit_card.save
+        format.html { redirect_to credit_cards_path, notice: 'Tarjeta de Crédito agregada.' }
+      else
+        format.html { render :new }
+      end#if/else
+    end#respond_to
+  end#create
+
+  def edit
+    #code
+  end#edit
+  def update
+    #code
+  end#update
+
+  def destroy
+    @credit_card.destroy
+    respond_to do |format|
+      format.html { redirect_to credit_cards_path, alert: 'Tarjeta eliminada.' }
+    end
+  end#destroy
+
+  protected
+  def set_credit_card
+    @credit_card = CreditCard.find(params[:id])
+  end#set_credit_card
+
+  def credit_card_params
+    params.require(:credit_card).permit(:user_id, :issuer, :kind, :expiration)
+  end#credit_card_params
+
+  def options_for_select
+    @kind_options = CreditCard.get_kind_options
+  end#options_for_select
+end#Controller
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,25 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
+  # Atributos: user, issuer, kind, expiration
+  ####### METHODS
+  def get_kind
+    kind = self.kind.blank? ? '' : self.kind
+    return kind
+  end
+  def get_issuer
+    issuer = self.issuer.blank? ?  '' : self.issuer
+    return issuer
+  end
+  def get_user_name
+    user = self.user.blank? ? '' : self.user.email
+    return user
+  end
+  def get_expiration
+    expiration = self.expiration.blank? ? '' : self.expiration
+    return expiration
+  end
+  ####### CLASS METHODS
+  def self.get_kind_options
+    return ['VISA','MastrerCard','Otra']
+  end
 end
